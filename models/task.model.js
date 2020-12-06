eacd5ebffc0114e037ec0b38176fd6f79b34fea2
Ilmira <tregulovailmira@gmail.com>
2020-12-06 15:49:18 +0200

    Add simple structure of task model

---
+++ models/task.model.js
@@ -0,0 +1,10 @@
+class Task {
+  //отвечает за обновление данных в контейнере, где они хранятся(массив, Map...)
+  static create(values) {}
+  static findById(id) {}
+  static findAll() {}
+  static update(id, values) {}
+  static delete(id) {}
+}
+
+module.exports = Task;
